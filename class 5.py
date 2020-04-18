d8831b3a8bb46d984f866c0cce8dbe9a229f8db0
cliff999 <61894486+cliff999@users.noreply.github.com>
2020-04-18 10:37:40 +0800

    Add files via upload

--- class 5.py
+++ class 5.py
@@ -11,8 +11,15 @@ res = requests.get('https://www.dcard.tw/service/api/v2/forums/pet/posts?limit=4
 data = json.loads(res.text)
 print(data)
 
+with open("test.json" , "w")as file:
+    json.dump(data, file)
 for item in data:
     print(item.get("title"))
     print(item.get("gender"))
     print(item.get("school"))
     print(item.get("topics"))
+file.close()
+with open("test.json", "r") as file:
+    jsonRead = json.load(file)
+
+file.close()
